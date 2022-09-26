b8c9fda0c401b1b0dd7aa04244fb4c78cb627565
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-26 08:14:15 -0300

    I init CHILLToken

---
+++ test/ChillToken.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "src/ChillToken.sol";
+
+contract ContractTest is Test {
+    ChillToken ct;
+    address minter;
+
+    function setUp() public {
+        minter = address(this);
+        ct = new ChillToken(minter);
+    }
+
+    function testCan_initVariables() public {
+        assertEq(ct.totalSupply(), 0);
+        assertEq(ct.name(), "CHILL");
+        assertEq(ct.symbol(), "CHILL");
+    }
+
+    function testCan_mintNewTokens() public {
+        ct.mint(address(this), 100);
+        assertEq(ct.totalSupply(), 100);
+    }
+
+    function testCan_revertNonMinterMintNewTokens() public {
+        vm.startPrank(address(1));
+        vm.expectRevert("not authorized to mint new $CHILL tokens");
+        ct.mint(address(this), 100);
+    }
+}
